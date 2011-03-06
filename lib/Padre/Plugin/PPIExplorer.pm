b92290d0eb21c19fc591cecbe678da2c4f8b55c7
2mad4milk <matthias@mad4milk.de>
2011-03-06 23:33:55 +0100

    Working version

--- lib/Padre/Plugin/PPIExplorer.pm
+++ lib/Padre/Plugin/PPIExplorer.pm
@@ -14,19 +14,16 @@ use Padre::Plugin  ();
 
 our @ISA = 'Padre::Plugin';
 
-# This constant is used when storing
-# and restoring the cursor position.
-# Keep it small to limit resource use.
+sub padre_interfaces {
+    'Padre::Plugin'     => '0.54',
+        'Padre::Config' => '0.54';
+}
+
 use constant {
     SELECTIONSIZE => 40,
     TRUE          => 1,
     FALSE         => undef,
 };
-
-sub padre_interfaces {
-    'Padre::Plugin'     => '0.54',
-        'Padre::Config' => '0.54';
-}
 
 sub plugin_name {
     Wx::gettext('Perl PPIExplorer');
@@ -35,23 +32,26 @@ sub plugin_name {
 sub menu_plugins_simple {
     my $self = shift;
     return $self->plugin_name => [
-        Wx::gettext("Dump the selected text")   => \&dump_selection,
-        Wx::gettext("Dump the active document") => \&dump_document,
+        Wx::gettext("Dump the selected text to the output") => \&dump_selection_to_output,
+        Wx::gettext("Dump the active text to the output")   => \&dump_document_to_output,
+        '---'                                               => undef,
+        Wx::gettext("Dump the selected text to new file") => \&dump_selection_to_newdoc,
+        Wx::gettext("Dump the active document to new file") => \&dump_document_to_newdoc,
     ];
 }
 
-# isa: function
-# no pod
+# returns: Boolean
+# description: print Error to output 
 sub _OnError {
     my $main = shift;
     $main->output->AppendText( "PPIExplorer Error:\n" . $@ );
     return FALSE;
 }
 
+
 # parameter: $main,$current,$source,$document
-# returns: TRUE or FALSE
-# description: raise error if error
-# isa: method
+# returns: String
+# description: Generates PPI-Output from document
 sub _dump {
     my $main     = shift;
     my $current  = shift;
@@ -59,62 +59,139 @@ sub _dump {
     my $document = $current->document;
 
     # Check for problems
-    unless ( defined $source ) { return FALSE };
-    
-    unless ( $document->isa('Padre::Document::Perl') ) {
-	my $ret = Wx::MessageBox(
-			"Document is not a Perl document. Should I try?",
-			"Warn",
-			3, 
-		$main,
-	);
-	unless ( $ret == 3 ){
-		return TRUE;
-	}
-    }
+    unless ( defined $source ) { return FALSE }
 
-    $main->show_output(1);
-    my $output = $main->output;
+    unless ( $document->isa('Padre::Document::Perl') ) {
+        my $ret = Wx::MessageBox( "Document is not a Perl document. Should I try?", "Warn", 3, $main, );
+        unless ( $ret == 3 ) {
+            return TRUE;
+        }
+    }
     require PPI;
     require PPI::Dumper;
 
     my $output_doc;
     eval {
-	   my $ppi_document = PPI::Document->new( \$source );
-       	   $output_doc = PPI::Dumper->new($ppi_document)->string;
+        my $ppi_document = PPI::Document->new( \$source );
+        $output_doc = PPI::Dumper->new($ppi_document)->string;
 
     } or do {
-            return _OnError($main,"PPIExplorer Error:\n".$@ ) if ($@);
-        };
-        
+        return _OnError( $main, "PPIExplorer Error:\n" . $@ ) if ($@);
+    };
+    return $output_doc;
+}
+
+sub _dump_to_output {
+    my $main       = shift;
+    my $current    = shift;
+    my $source     = shift;
+    my $document   = $current->document;
+    my $output_doc = _dump( $main, $current, $source );
+    $main->show_output(1);
+    my $output = $main->output;
+
     if ($output_doc) {
-	$output->clear;
+        $output->clear;
         $output->AppendText("$output_doc");
     }
 
     return TRUE;
 }
 
-#POD: dump the current selected text
-sub dump_selection {
+sub _dump_to_doc {
     my $main = shift;
+    my $current    = shift;
+    my $source     = shift;
+    my $document   = $current->document;
+    my $output_doc = _dump( $main, $current, $source );
+    my $filename = _get_filename($main);
+    return unless defined $filename;
+    
+    if ($output_doc) {
+        $main->show_output(1);
+        open my $filehandle , '>',$filename  or ( $main->output->AppendText("Fehler beim offnen der Datei -$filename-\n"   .$@ )  and return FALSE);
+        print $filehandle $output_doc        or ( $main->output->AppendText("Fehler beim schreiben in Datei -$filename-\n" .$@ )  and return FALSE);
+        close $filehandle                    or ( $main->output->AppendText( "Fehler beim schließen der Datei -$filename-\n".$@ ) and return FALSE);
+        #$main->output->clear;
+        $main->output->AppendText("In Datei geschrieben!");
+    }
+    return TRUE;
+
+}
+sub _get_filename {
+	my $main = shift;
+	my $doc         = $main->current->document or return;
+	my $current     = $doc->filename;
+	my $default_dir = '';
+
+	if ( defined $current ) {
+		require File::Basename;
+		$default_dir = File::Basename::dirname($current);
+	}
+
+	require File::Spec;
+
+	while (1) {
+		my $dialog = Wx::FileDialog->new(
+			$main, Wx::gettext("Save file as..."),
+			$default_dir, ( $current or $doc->get_title ) . '.txt',
+			"*.*", Wx::wxFD_SAVE,
+		);
+		if ( $dialog->ShowModal == Wx::wxID_CANCEL ) {
+			return FALSE;
+		}
+		my $filename = $dialog->GetFilename;
+		$default_dir = $dialog->GetDirectory;
+		my $path = File::Spec->catfile( $default_dir, $filename );
+		if ( -e $path ) {
+			return $path if $main->yes_no( Wx::gettext("File already exists. Overwrite it?"), Wx::gettext("Exist") );
+		} else {
+			return $path;
+		}
+	}
+}
+
+
+#POD: dump the current selected text
+sub dump_selection_to_output {
+    my $main    = shift;
     my $current = $main->current;
     my $text    = $current->text;
-    my $dump    = _dump( $main, $current, $text );
+    _dump_to_output( $main, $current, $text );
+    return TRUE;
+}
+
+#POD: dump the entire current document
+sub dump_document_to_output {
+    my $main     = shift;
+    my $current  = $main->current;
+    my $document = $current->document;
+    my $text     = $document->text_get;
+    _dump_to_output( $main, $current, $text );
+    return TRUE;
+}
+
+#POD: dump the current selected text
+sub dump_selection_to_newdoc {
+    my $main    = shift;
+    my $current = $main->current;
+    my $text    = $current->text;
+    my $dump    = _dump_to_doc( $main, $current, $text );
     unless ( defined Params::Util::_STRING($dump) ) {
         return FALSE;
     }
+
     #$current->editor->ReplaceSelection($dump);
     return TRUE;
 }
 
 #POD: dump the entire current document
-sub dump_document {
-    my $main = shift;
+sub dump_document_to_newdoc {
+    my $main     = shift;
     my $current  = $main->current;
     my $document = $current->document;
     my $text     = $document->text_get;
-    my $dump     = _dump( $main, $current, $text );
+    my $dump     = _dump_to_doc( $main, $current, $text );
     unless ( defined Params::Util::_STRING($dump) ) {
         return FALSE;
     }
